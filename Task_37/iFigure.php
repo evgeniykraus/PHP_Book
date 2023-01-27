814e0875eaed4b82c778392efb28d517c7a7aca6
evgeniykraus <evgeniykraus@gmail.com>
2023-01-27 19:05:07 +0700

    Done

---
+++ Task_37/iFigure.php
@@ -0,0 +1,8 @@
+<?php
+
+interface iFigure
+{
+    public function getSquare(): float;
+
+    public function getPerimeter(): float;
+}
---
+++ Task_37/iTetragon.php
@@ -0,0 +1,12 @@
+<?php
+
+interface iTetragon
+{
+    public function getA(): float;
+
+    public function getB(): float;
+
+    public function getC(): float;
+
+    public function getD(): float;
+}
