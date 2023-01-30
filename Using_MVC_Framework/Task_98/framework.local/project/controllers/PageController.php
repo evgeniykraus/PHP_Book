60211ee7c3a5499c3ecec9bd315443f6b5f7396e
Краус Евгений Владимирович <evgeniykraus@gmail.com>
2023-01-30 11:52:57 +0700

    Done

---
+++ Using_MVC_Framework/Task_98/framework.local/project/controllers/PageController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Project\Controllers;
+
+use \Core\Controller;
+
+class PageController extends Controller
+{
+    private $pages;
+
+    public function __construct()
+    {
+        $this->pages = [
+            1 => ['title' => 'страница 1', 'text' => 'текст страницы 1'],
+            2 => ['title' => 'страница 2', 'text' => 'текст страницы 2'],
+            3 => ['title' => 'страница 3', 'text' => 'текст страницы 3'],
+        ];
+    }
+
+    public function act()
+    {
+        $this->title = 'Действие act контроллера page';
+
+        return $this->render('page/act', [
+            'header' => 'список юзеров',
+            'users' => ['user1', 'user2', 'user3'],
+        ]);
+    }
+
+    public function show($params)
+    {
+        $this->title = $this->pages[$params['n']]['title'];
+
+        return $this->render('page/show', [
+            'text' => $this->pages[$params['n']]['text']
+        ]);
+    }
+}
